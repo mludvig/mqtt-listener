4be32a02e7a1fa2110b57a41fea4e9b8de8f3061
Michael Ludvig <mludvig@logix.net.nz>
2019-05-19 14:18:31 +1200

    Improved sample-script.sh

--- sample-script.sh
+++ sample-script.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# The mqtt-listener will pass two environment variables:
+#
+# MQTT_TOPIC   - the topic name where the message was received from,
+#                e.g. iotbutton/G123ABCD1234ABCD
+#
+# MQTT_PAYLOAD - Message received from the topic, usually JSON
+#                formatted in which case 'jq' can be used to parse it.
+#                e.g. '{ "serialNumber": "G123ABCD1234ABCD", "clickType": "SINGLE" }'
+
 echo "MQTT_TOPIC: ${MQTT_TOPIC}"
 echo "MQTT_PAYLOAD: ${MQTT_PAYLOAD}"
 
-SERIAL_NUMBER=$(jq -r .serialNumber <<< ${MQTT_PAYLOAD})
+# Use 'jq' to parse JSON payload
+# e.g. to extract "clickType" from AWS IoT Button message
+CLICK_TYPE=$(jq -r .clickType <<< ${MQTT_PAYLOAD})
 
-notify-send --urgency=normal --expire-time 10000 --icon error "MQTT Message from ${MQTT_TOPIC}" "${MQTT_PAYLOAD}\n${SERIAL_NUMBER}"
+notify-send --urgency=normal --icon info "${CLICK_TYPE} click message from ${MQTT_TOPIC}" "${MQTT_PAYLOAD}"
